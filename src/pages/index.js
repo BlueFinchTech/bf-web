ba7d3fe088673a7c69cf1a7baee9cd85982f854a
Andrew B <andrew.b@bluefinch.uk>
2022-06-17 21:59:01 +0100

    Fixed button on safari

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
         <h2 class="px-5 pb-5 font-thin text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Technologies Ltd</h2>
         <p class="text-slate-50 p-5">A bespoke software development house, based in the UK.</p>
         <span class="pr-10">
-          <a href="mailto:info@bluefinch.uk" class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+          <a href="mailto:info@bluefinch.uk" class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
             Get in touch
           </a>
         </span>
